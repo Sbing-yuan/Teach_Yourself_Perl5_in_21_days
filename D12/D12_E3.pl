5f91670fb78deec56ff7cef0f022e9eb5761e6c2
john <qaznsengoku@hotmail.com>
2024-03-02 00:42:07 +0800

    D12_E3.pl re-commit

--- D12/D12_E3.pl
+++ D12/D12_E3.pl
@@ -1,31 +1,39 @@
 #!/usr/local/bin/perl
 
-$homedir = "/mnt/d/git repo/Teach_Yourself_Perl5_in_21_days/test";
+#$homedir = "/mnt/d/git repo/Teach_Yourself_Perl5_in_21_days/test";
+#$homedir = "/mnt/c/git repo/Teach_Yourself_Perl5_in_21_days/test";
+$homedir = "/mnt/c/git repo/Teach_Yourself_Perl5_in_21_days";
 
 opendir(MYDIR, $homedir);
 
-for (my $i=0; $i<10; $i++) {
+while (1) {
 	while($file = readdir(MYDIR)) {
 		if($file =~ /^\./) { #ignore files start with .
 			next;	
 		}
 		elsif($curr_highest eq "") {
-			$curr_highest = ($file gt $prev_highest) ? $curr_highest : $file;
+				$curr_highest = $file;
 		}
-		elsif($file gt $curr_highest) {
-			if($prev_highest eq "") {
-				$curr_highest = $file;	
-			}
-			else {
-				$curr_highest = ($file gt $prev_highest) ? $curr_highest : $file;		
-			}
+		elsif($file gt $curr_highest  && $prev_highest eq "") {
+				$curr_highest = $file;
 		}
-		elsif($curr_highest gt $file) {
-			$curr_highest = ($file gt $prev_highest) ? $curr_highest : $file;		
+		elsif($file gt $curr_highest  && $prev_highest gt $file) {
+				$curr_highest = $file;
 		}
 	}
+	if($curr_highest eq $prev_highest) {
+		last;
+	}
+
 	print("$curr_highest\n");
 	$prev_highest = $curr_highest;
 	$curr_highest = "";
 	rewinddir(MYDIR);
 }
+
+
+#while($file = readdir(MYDIR)) {
+#	print("$file\n");
+#}
+
+
