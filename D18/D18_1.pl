8b3492fedb250cfdabfa67820ba264e94f099859
Sbing-yuan <62290987+Sbing-yuan@users.noreply.github.com>
2024-03-21 18:09:46 +0800

    Create D18_1.pl

---
+++ D18/D18_1.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+#
+# Using Array references
+#
+$pointer = \@ARGV;
+printf "\n Pointer Address of ARGV = $pointer\n";
+$i = scalar(@$pointer);
+printf "\n Number of arguments : $i \n";
+$i = 0;
+ foreach (@$pointer) {
+ printf "$i : $$pointer[$i++]; \n";
+ }
