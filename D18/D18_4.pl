03b2dd9e85c18ab263a6cd05eb66dcfc7289f09d
Sbing-yuan <62290987+Sbing-yuan@users.noreply.github.com>
2024-03-21 18:12:27 +0800

    Create D18_4.pl

---
+++ D18/D18_4.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+#
+# Using Multi-dimensional Array references
+#
+$line = ['solid', 'black', ['1','2','3'] , ['4', '5', '6']];
+print "\$line->[0] = $line->[0] \n";
+print "\$line->[1] = $line->[1] \n";
+print "\$line->[2][0] = $line->[2][0] \n";
+print "\$line->[2][1] = $line->[2][1] \n";
+print "\$line->[2][2] = $line->[2][2] \n";
+print "\$line->[3][0] = $line->[3][0] \n";
+print "\$line->[3][1] = $line->[3][1] \n";
+print "\$line->[3][2] = $line->[3][2] \n";
+print "\n"; # The obligatory output beautifier.
